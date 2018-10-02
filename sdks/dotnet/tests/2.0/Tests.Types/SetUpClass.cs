ea04a8219ec7f48f44f7748ed3678fca6e7a1fd8
Renato Golia <renato.golia@educations.com>
2018-10-02 17:31:38 +0200

    Remove namespace for SetUpClass

--- sdks/dotnet/tests/2.0/Tests.Types/SetUpClass.cs
+++ sdks/dotnet/tests/2.0/Tests.Types/SetUpClass.cs
@@ -5,31 +5,29 @@ using NUnit.Framework;
 
 // ReSharper disable CheckNamespace
 
+[SetUpFixture]
+public class SetUpClass
+{
+    private static readonly Uri SchemaUri = new Uri(@"https://raw.githubusercontent.com/emgdev/xml-import/master/schemas/2.0/xml-import.xsd", UriKind.Absolute);
 
-namespace Tests.EMG20 {
-    [SetUpFixture]
-    public class SetUpClass
+    private static XmlSchemaSet GetXmlSchema()
     {
-        private static readonly Uri SchemaUri = new Uri(@"https://raw.githubusercontent.com/emgdev/xml-import/master/schemas/2.0/xml-import.xsd", UriKind.Absolute);
-
-        private static XmlSchemaSet GetXmlSchema()
+        XmlSchemaSet schemaSet = new XmlSchemaSet { XmlResolver = new XmlUrlResolver() };
+        using (XmlTextReader reader = new XmlTextReader(SchemaUri.ToString()))
         {
-            XmlSchemaSet schemaSet = new XmlSchemaSet { XmlResolver = new XmlUrlResolver() };
-            using (XmlTextReader reader = new XmlTextReader(SchemaUri.ToString()))
-            {
-                var schema = XmlSchema.Read(reader, null);
-                schemaSet.Add(schema);
-            }
-
-            schemaSet.Compile();
-
-            return schemaSet;
+            var schema = XmlSchema.Read(reader, null);
+            schemaSet.Add(schema);
         }
 
-        [OneTimeSetUp]
-        public void FirstInitialization()
-        {
-            XmlSchemas.XmlImport = GetXmlSchema();
-        }
+        schemaSet.Compile();
+
+        return schemaSet;
+    }
+
+    [OneTimeSetUp]
+    public void FirstInitialization()
+    {
+        XmlSchemas.XmlImport = GetXmlSchema();
     }
 }
+
